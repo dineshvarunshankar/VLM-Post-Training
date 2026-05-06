e0f5e5c7b40bf54856b2cb31b0fe969476f3f31a
dineshvarunshankar <dineshvarunshankar@gmail.com>
2026-05-06 19:59:49 -0400

    latest unsloth compatibility for both scripts - cosmos2, gemma

--- data_processor.py
+++ data_processor.py
@@ -1,14 +1,13 @@
 import json
-import os
 
 class DataProcessor:
     def __init__(self, input_jsonl_file):
         self.input_file = input_jsonl_file
 
-    def process_and_save_gemma4(self, output_file="outputs/dataset/sft_gemma4.jsonl"):
+    def process_and_save(self, output_file="outputs/dataset/sft_model_name.jsonl"):
 
-        with open(self.input_file, 'r', encoding='utf-8') as f_in, \
-             open(output_file, 'w', encoding='utf-8') as f_out:
+        with open(self.input_file, 'r') as f_in, \
+             open(output_file, 'w') as f_out:
             
             for line in f_in:
                 if not line.strip():
@@ -16,7 +15,7 @@ class DataProcessor:
                     
                 data = json.loads(line)
 
-                # Format the reasoning block for Gemma 4
+                # Format the reasoning block 
                 cot = data.get('cot', '')
                 answer = data.get('answer', '').capitalize()
                 reasoning_answer = f"<|channel>thought\n{cot}\n<channel|>\n{answer}."
@@ -54,3 +53,7 @@ class DataProcessor:
                 
         print(f"Data conversion complete. Saved to: {output_file}")
         return output_file
+
+if __name__ == "__main__":
+    processor = DataProcessor("exports/outputs/sft.jsonl")
+    processor.process_and_save()
---
+++ finetune_Gemma4.py
@@ -0,0 +1,168 @@
+import numpy as np
+import unsloth
+from trl import SFTTrainer, SFTConfig
+from data_processor import DataProcessor
+from unsloth import FastModel
+from unsloth.trainer import UnslothVisionDataCollator
+from unsloth.chat_templates import get_chat_template, standardize_data_formats
+from datasets import load_dataset
+import torch
+import os
+import datetime
+#for wandb project
+os.environ["WANDB_PROJECT"] = "Triage-VLM-Post-Training"
+
+#datetime string
+datetime_string = datetime.now().strftime('%Y%m%d_%H%M%S')
+
+#Available gemma4 models
+
+gemma4_models = [
+    # Gemma-4 instruct models:
+    "unsloth/gemma-4-E2B-it",
+    "unsloth/gemma-4-E4B-it",
+    "unsloth/gemma-4-31B-it",
+    "unsloth/gemma-4-26B-A4B-it",
+    # Gemma-4 base models:
+    "unsloth/gemma-4-E2B",
+    "unsloth/gemma-4-E4B",
+    "unsloth/gemma-4-31B",
+    "unsloth/gemma-4-26B-A4B",
+]
+
+#load the model
+model, tokenizer = FastModel.from_pretrained(
+    model_name = "unsloth/gemma-4-31B-it",
+    dtype = torch.bfloat16, # None for auto detection or torch.float32, torch.float16, torch.bfloat16, etc.
+    max_seq_length = 8192, # context window - prompt + image tokens + generated response
+    load_in_4bit = True,  # 4 bit quantization to reduce memory (NF4)
+    load_in_8bit = False, # 8 bit quantization to reduce memory (Q8_0)
+    load_in_16bit = False, # 16 bit quantization(F16)
+    load_in_fp8 = False, # 16 bit quantization(BF16)
+    full_finetuning = False, # full finetuning 
+    # token = "YOUR_HF_TOKEN", # HF Token for gated models
+    device_map = "balanced" # for equally distributed multi-GPU training. "auto" for auto-find and spread. "cuda:0" for single GPU training.
+)
+
+#finetune gemma 4
+
+model = FastModel.get_peft_model(
+    model,
+    finetune_vision_layers     = True, # Turn off for just text!
+    finetune_language_layers   = True,  # Should leave on!
+    finetune_attention_modules = True,  # Attention good for GRPO
+    finetune_mlp_modules       = True,  # Should leave on always!
+    r = 8,           # Larger = higher accuracy, but might overfit
+    lora_alpha = 8,  # Recommended alpha == r at least
+    lora_dropout = 0.05,
+    bias = "none",
+    random_state = 3407, #a paper proved it was optimal - https://arxiv.org/abs/2109.08203  
+    use_rslora = False,  # rank stabilized LoRA - /sqrt(r)
+    loftq_config = None, #  LoftQ - Calculates what was lost during compression and preloads it into LoRA adapter.
+    # target_modules = "all-linear", # Optional now! Can specify a list if needed
+)
+
+#Data preparation
+processor = DataProcessor("exports/outputs/sft.jsonl")
+ready_file = processor.process_and_save()
+dataset = load_dataset("json", data_files=ready_file, split="train")
+
+#get gemmma4 template - convert to jinja formatting script and inject it into the tokenizer
+tokenizer = get_chat_template(
+    tokenizer,
+    chat_template="gemma-4-thinking",
+)
+
+#standardize data formats - Hugging Face JSON to conversational arrays
+dataset = standardize_data_formats(dataset)
+
+#apply chat template - flatten the conversations into a single text field - Only for text finetuning
+# def formatting_prompts_func(examples):
+#     convos = examples["conversations"]
+#     texts = [tokenizer.apply_chat_template(convo, tokenize = False, add_generation_prompt = False).removeprefix('<bos>') for convo in convos]
+#     return { "text" : texts, }
+
+# dataset = dataset.map(formatting_prompts_func, batched = True)
+
+#Train the model
+
+trainer = SFTTrainer(
+    model=model,
+    tokenizer=tokenizer,
+    data_collator=UnslothVisionDataCollator(
+        model,
+        tokenizer,
+        train_on_responses_only = True,
+        # ChatML specific masking for Gemma-4 models:
+        instruction_part = "<|turn>user\n",
+        response_part = "<|turn>model\n",
+        completion_only_loss = True,
+    ),
+    train_dataset=dataset,
+    eval_dataset=None,
+    args=SFTConfig(
+        #dataset_text_field="text", #commented out for vision finetuning
+        per_device_train_batch_size=1,
+        gradient_accumulation_steps=4,
+        warmup_steps=5,
+        num_train_epochs=1,
+        max_steps=None, # only for testing. set to None if you do full run.
+        learning_rate=2e-4,
+        logging_steps=1,
+        optim="adamw_8bit",
+        weight_decay=0.001,
+        lr_scheduler_type="linear",
+        seed=3407,
+        report_to="wandb",
+        run_name=f"gemma_4/lora_{datetime_string}",
+        output_dir=f"outputs/gemma_4/lora_{datetime_string}",
+
+        # You MUST put the below items for vision finetuning:
+        remove_unused_columns = False, # by default, it looks at text field, if we have images, it would ignore it. Forcing this to false protects images.
+        dataset_text_field = "", #stops looking for text field since we have images.
+        dataset_kwargs = {"skip_prepare_dataset": True}, # tells HF to skip data processing since unsloth already processed it.
+        max_length = 8192, #HF leaves room for unsloth to add tokens. Should match "max_seq_length" in the model.from_pretrained()
+    )
+)
+
+# #Train on assistant outputs and ignore the loss on the user's inputs
+# trainer = train_on_responses_only(
+#     trainer,
+#     instruction_part="<|turn>user\n",
+#     response_part="<|turn>model\n",
+# )
+
+#memory stats - initial
+
+gpu_stats = torch.cuda.get_device_properties(0)
+start_gpu_memory = round(torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3)
+max_memory = round(gpu_stats.total_memory / 1024 / 1024 / 1024, 3)
+print(f"GPU = {gpu_stats.name}. Max memory = {max_memory} GB.")
+print(f"{start_gpu_memory} GB of memory reserved.")
+
+#TRAINING
+trainer_stats = trainer.train()
+
+#memory stats - final
+used_memory = round(torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3)
+used_memory_for_lora = round(used_memory - start_gpu_memory, 3)
+used_percentage = round(used_memory / max_memory * 100, 3)
+lora_percentage = round(used_memory_for_lora / max_memory * 100, 3)
+print(f"{trainer_stats.metrics['train_runtime']} seconds used for training.")
+print(f"{round(trainer_stats.metrics['train_runtime']/60, 2)} minutes used for training.")
+print(f"Peak reserved memory = {used_memory} GB.")
+print(f"Peak reserved memory for training = {used_memory_for_lora} GB.")
+print(f"Peak reserved memory % of max memory = {used_percentage} %.")
+print(f"Peak reserved memory for training % of max memory = {lora_percentage} %.")
+
+#save the model
+model.save_pretrained(f"outputs/gemma_4/lora_{datetime_string}/adapter_weights")
+tokenizer.save_pretrained(f"outputs/gemma_4/lora_{datetime_string}/tokenizer_weights")
+# model.push_to_hub("HF_ACCOUNT/gemma_4_lora", token = "YOUR_HF_TOKEN") # Online saving
+# tokenizer.push_to_hub("HF_ACCOUNT/gemma_4_lora", token = "YOUR_HF_TOKEN") # Online saving
+
+#saving to float16 for VLLM
+model.save_pretrained_merged(f"outputs/gemma_4/lora_{datetime_string}/fused_model_weights", tokenizer)
+
+#saving to GGUF/llama.cpp conversion
+#model.save_pretrained_gguf(f"outputs/gemma_4/lora_{datetime_string}/fused_model_weights", tokenizer, quantization_method = "Q8_0") # Q8_0, BF16, F16 supported
